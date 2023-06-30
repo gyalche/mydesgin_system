48fac2e70a9a6e593a7c3d9b03b0acf574182b92
GAUDREAU FRANCIS <gaudreaufrancis@GAUDREAUnoMacBook-Pro.local>
2023-06-30 14:16:36 +0900

    added eslint and some basic rules

--- src/atoms/Buttons/Button.stories.js
+++ src/atoms/Buttons/Button.stories.js
@@ -1,33 +1,31 @@
-import { Button } from './Button';
-import { withDesign } from 'storybook-addon-designs'
+import { Button } from "./Button";
+import { withDesign } from "storybook-addon-designs";
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
-    Somearguments: {control: 'text'}
+    backgroundColor: { control: "color" }
   },
   // Figma integration, import the url of the related figma design
   decorators: [withDesign],
   parameters: {
     design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/7GhAI7t2dM3tVWpWMAVFXJ/Evergreen-v6-(Community)-(Copy)?type=design&node-id=6044-15271&mode=design&t=9NHRiv6uwxtagOEf-4',
-   },
-
+      type: "figma",
+      url: "https://www.figma.com/file/7GhAI7t2dM3tVWpWMAVFXJ/Evergreen-v6-(Community)-(Copy)?type=design&node-id=6044-15271&mode=design&t=9NHRiv6uwxtagOEf-4"
+    }
   }
 };
 
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
-  },
+    label: "Button"
+  }
 };
 
 export const Secondary = {
   args: {
-    label: 'Button',
-  },
+    label: "Button"
+  }
 };
