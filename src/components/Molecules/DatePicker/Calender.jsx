2e6cb895a268fe76efb2cd08e6dcd5a8acc7c51c
Dawa Sherpa <fupulamu01@gmail.com>
2024-10-21 16:53:51 +0545

    feat: when EndCalender is open the startDate selection will be disabled, so that users dont select the endDate that is less than start date

--- src/components/Molecules/DatePicker/Calender.jsx
+++ src/components/Molecules/DatePicker/Calender.jsx
@@ -70,7 +70,7 @@ const Calendar = ({
               isKeyboardSelect={normalizeDate(dayDate) === normalizeDate(isSelected) 
                 && normalizeDate(dayDate) !== normalizeDate(startDate)}
               isInRange={isInRange(dayDate)}
-              isDisabled={normalizeDate(dayDate) < normalizeDate(new Date()) || notCurrent}
+              isDisabled={normalizeDate(dayDate) < normalizeDate(new Date()) || normalizeDate(dayDate) < normalizeDate(startDate)}
               isSaturday={dayOfWeek === 5}
               isSunday={dayOfWeek === 6}
               onClick={!isRangePicker ? () => handleSingleDate(dayDate) : () => handleDateRangeClick(dayDate)}
