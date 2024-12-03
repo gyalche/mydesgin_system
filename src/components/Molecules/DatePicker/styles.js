9e32c52902ac7795b40d6c07a0b6c926eaea2846
Dawa Sherpa <fupulamu01@gmail.com>
2024-12-03 11:59:38 +0545

    [CSS]: setup bg for calender

--- src/components/Molecules/DatePicker/styles.js
+++ src/components/Molecules/DatePicker/styles.js
@@ -97,6 +97,7 @@ export const CalendarContainer = styled.div`
   width: 340px;
   height: 320px; 
   padding: 10px;
+  background-color: var(--rds-color-neutral-0);
 `;
 
 export const DaysContainer = styled.div`
