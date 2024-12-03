eba4db3555bfd898677501fe3ac2cf6a78a687bc
Dawa Sherpa <fupulamu01@gmail.com>
2024-12-03 14:12:05 +0545

    [CSS]: dropdown bg

--- src/components/Molecules/DatePicker/styles.js
+++ src/components/Molecules/DatePicker/styles.js
@@ -248,6 +248,7 @@ export const Dropdown = styled.div`
   overflow-y: hidden;
   box-shadow: 0px 4px 8px 0px var(--rds-color-neutral-5);
   margin-top: 18px;
+  background-color: var(--rds-color-neutral-0);
 `;
 export const EndDropDown = styled(Dropdown)`
   margin-left: ${({ is12Hour }) => {
