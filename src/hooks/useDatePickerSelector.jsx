55a04e7fcddfa7c84f9ae71209df48f5b23a340d
Dawa Sherpa <fupulamu01@gmail.com>
2025-01-21 13:04:56 +0545

    [REFACTOR]: fixed naming error notCurrentMonthAndYear

--- src/hooks/useDatePickerSelector.jsx
+++ src/hooks/useDatePickerSelector.jsx
@@ -32,7 +32,7 @@ function useDatePickerSelector({
   const [displayErrorLast, setDisplayErrorLast] = useState(false);
   const [enableKeyboard, setEnableKeyboard] = useState(true);
 
-  const notCurrentMontAndYear = currentDate.getMonth() !== currentMonth.getMonth() || currentDate.getFullYear() !== currentMonth.getFullYear();
+  const notCurrentMonthAndYear = currentDate.getMonth() !== currentMonth.getMonth() || currentDate.getFullYear() !== currentMonth.getFullYear();
 
   const datePickerRef = useRef(null);
   const inputRefEnd = useRef(null);
@@ -104,6 +104,7 @@ function useDatePickerSelector({
     if(dateTimeStart) setDateTimeStart(false);
     if(dateTimeEnd) setDateTimeEnd(false);
   };
+
   const clearStartDateWhenNoDateTime = () => {
     setStartDate('');
     setDisplayErrorFirst(true);
@@ -126,7 +127,6 @@ function useDatePickerSelector({
     setHoveredDate(startDate);
   };
 
- 
   const isInHoverRange = useCallback((day) => {
     if (!startDate || !hoveredDate) return false;
     const normalizedDay = normalizeDate(day);
@@ -182,92 +182,6 @@ function useDatePickerSelector({
     if(!(val instanceof Date)) return;
     setCurrentMonth(val);
   }, [setCurrentMonth]);
-
-  const handleKeyDown = (e) => {
-    e.preventDefault();
-    const today = new Date();
-    const todayNormalized = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-    
-    const updateDate = (changeFn) => {
-      setCurrentDate((prev) => {
-        const newDate = changeFn(prev);
-        const newYear = newDate.getFullYear();
-        const currentYear = currentMonth.getFullYear();
-        const isNextMonth = newDate.getMonth() > currentMonth.getMonth();
-        const isPrevMonth = newDate.getMonth() < currentMonth.getMonth();
-
-        if (isNextMonth && newYear === currentYear) handleNextMonth();
-        if (isPrevMonth && newYear === currentYear) handlePrevMonth();
-        if (newYear > currentYear) handleNextMonth();
-        if (newYear < currentYear) handlePrevMonth();
-
-        return onlyFuture ? newDate < todayNormalized ? todayNormalized : newDate : newDate;
-      });
-    };
-
-    const handleEnter = () => {
-      e.preventDefault();
-      e.stopPropagation();
-    
-      if (openCalendar) {
-        if (isRangePicker) {
-          if (!startDate) {
-            setStartDate(currentDate);
-          } else if (endDate || currentDate < startDate) {
-            setStartDate(currentDate);
-          } else {
-            setEndDate(currentDate);
-            setHoveredDate(currentDate);
-            setOpenCalendarEnd(false);
-            input.onChange([startDate, currentDate]);
-            onChange([startDate, currentDate]);
-            return;
-          }
-          setOpenCalendar(false);
-          setOpenCalendarEnd(true);
-          inputRefEnd.current?.focus();
-        } else {
-          setStartDate(currentDate);
-          input.onChange(currentDate);
-          onChange(currentDate);
-          setOpenCalendar(false);
-        }
-      } else if (openCalendarEnd) {
-        if (currentDate >= startDate) {
-          setEndDate(currentDate);
-          setHoveredDate(currentDate);
-          setOpenCalendarEnd(false);
-          input.onChange([startDate, currentDate]);
-          onChange([startDate, currentDate]);
-        }
-      }
-    };
-
-    switch (e.key) {
-      case 'ArrowLeft':
-        selectPreviousDate(notCurrentMontAndYear);
-        updateDate((prev) => new Date(prev.setDate(prev.getDate() - 1)));
-        break;
-      case 'ArrowRight':
-        selectPreviousDate(notCurrentMontAndYear);
-        updateDate((prev) => new Date(prev.setDate(prev.getDate() + 1)));
-        break;
-      case 'ArrowUp':
-        selectPreviousDate(notCurrentMontAndYear);
-        updateDate((prev) => new Date(prev.setDate(prev.getDate() - 7)));
-        break;
-      case 'ArrowDown':
-        selectPreviousDate(notCurrentMontAndYear);
-        updateDate((prev) => new Date(prev.setDate(prev.getDate() + 7)));
-        break;
-      case 'Enter':
-        if (!isRangePicker || dateTimeValue) handleSingleDate(currentDate);
-        handleEnter(e);
-        break;
-      default:
-        break;
-    }
-  };
 
   const disableKeyboardFunc = () => {
     setEnableKeyboard(false);
@@ -335,6 +249,92 @@ function useDatePickerSelector({
     }
   };
 
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const today = new Date();
+    const todayNormalized = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    
+    const updateDate = (changeFn) => {
+      setCurrentDate((prev) => {
+        const newDate = changeFn(prev);
+        const newYear = newDate.getFullYear();
+        const currentYear = currentMonth.getFullYear();
+        const isNextMonth = newDate.getMonth() > currentMonth.getMonth();
+        const isPrevMonth = newDate.getMonth() < currentMonth.getMonth();
+
+        if (isNextMonth && newYear === currentYear) handleNextMonth();
+        if (isPrevMonth && newYear === currentYear) handlePrevMonth();
+        if (newYear > currentYear) handleNextMonth();
+        if (newYear < currentYear) handlePrevMonth();
+
+        return onlyFuture ? newDate < todayNormalized ? todayNormalized : newDate : newDate;
+      });
+    };
+
+    const handleEnter = () => {
+      e.preventDefault();
+      e.stopPropagation();
+    
+      if (openCalendar) {
+        if (isRangePicker) {
+          if (!startDate) {
+            setStartDate(currentDate);
+          } else if (endDate || currentDate < startDate) {
+            setStartDate(currentDate);
+          } else {
+            setEndDate(currentDate);
+            setHoveredDate(currentDate);
+            setOpenCalendarEnd(false);
+            input.onChange([startDate, currentDate]);
+            onChange([startDate, currentDate]);
+            return;
+          }
+          setOpenCalendar(false);
+          setOpenCalendarEnd(true);
+          inputRefEnd.current?.focus();
+        } else {
+          setStartDate(currentDate);
+          input.onChange(currentDate);
+          onChange(currentDate);
+          setOpenCalendar(false);
+        }
+      } else if (openCalendarEnd) {
+        if (currentDate >= startDate) {
+          setEndDate(currentDate);
+          setHoveredDate(currentDate);
+          setOpenCalendarEnd(false);
+          input.onChange([startDate, currentDate]);
+          onChange([startDate, currentDate]);
+        }
+      }
+    };
+
+    switch (e.key) {
+      case 'ArrowLeft':
+        selectPreviousDate(notCurrentMonthAndYear);
+        updateDate((prev) => new Date(prev.setDate(prev.getDate() - 1)));
+        break;
+      case 'ArrowRight':
+        selectPreviousDate(notCurrentMonthAndYear);
+        updateDate((prev) => new Date(prev.setDate(prev.getDate() + 1)));
+        break;
+      case 'ArrowUp':
+        selectPreviousDate(notCurrentMonthAndYear);
+        updateDate((prev) => new Date(prev.setDate(prev.getDate() - 7)));
+        break;
+      case 'ArrowDown':
+        selectPreviousDate(notCurrentMonthAndYear);
+        updateDate((prev) => new Date(prev.setDate(prev.getDate() + 7)));
+        break;
+      case 'Enter':
+        if (!isRangePicker || dateTimeValue) handleSingleDate(currentDate);
+        handleEnter(e);
+        break;
+      default:
+        break;
+    }
+  };
+
   useEffect(() => {
     if ((openCalendar || openCalendarEnd) && enableKeyboard && !disabled) {
       window.addEventListener('keydown', handleKeyDown);
@@ -343,7 +343,7 @@ function useDatePickerSelector({
         window.removeEventListener('keydown', handleKeyDown);
       };
     }
-  }, [currentDate, currentMonth, openCalendar, openCalendarEnd, enableKeyboard, hoveredDate, startDate, endDate, notCurrentMontAndYear]);
+  }, [currentDate, currentMonth, openCalendar, openCalendarEnd, enableKeyboard, hoveredDate, startDate, endDate, notCurrentMonthAndYear]);
 
   return {
     startDate,
