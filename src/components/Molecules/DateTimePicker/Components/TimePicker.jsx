018167bab3c8d03852a4ee9e7c60b17c62870fa8
Dawa Sherpa <fupulamu01@gmail.com>
2025-02-11 09:32:20 +0545

    [Rename] timePerios -> timePeriod

--- src/components/Molecules/DateTimePicker/Components/TimePicker.jsx
+++ src/components/Molecules/DateTimePicker/Components/TimePicker.jsx
@@ -14,7 +14,7 @@ import {
   TimePickerContainer,
 } from '../styles';
 import InputField from './InputField';
-import { AM_PM_VALUES } from '../../../../constant/timePerios';
+import { AM_PM_VALUES } from '../../../../constant/timePeriod';
 import { useTimePickerKeyboardNavigation } from '../hooks/useTimePickerKeyboardNavigation';
 import { useTimePickerHandler } from '../hooks/useTimePickerHandler';
 import IconWithCalendar from './IconWithCalendar';
