3f1478ffe82e2c4fdabb7e8a1cd5bc9354e9f674
Dawa Sherpa <fupulamu01@gmail.com>
2025-03-26 10:30:37 +0545

    minor change

--- stories/tools/FormComponentTester/FormComponentTester.stories.jsx
+++ stories/tools/FormComponentTester/FormComponentTester.stories.jsx
@@ -105,7 +105,7 @@ const componentConfigs = {
   DateTimePicker: {
     component: DateTime,
     fieldName: 'dateTimePickerField',
-    initialValue: new Date(),
+    initialValue: new Date('Dec 20 2025 1:50:00'),
   },
 };
 
@@ -212,7 +212,7 @@ export const FormComponentTester = {
                   <Layout.Flex gap="24px">
                     <Layout.Item flex="1">
                       <Layout.Flex direction="column" gap="16px">
-                        {['RadioButton', 'TimePicker', 'DatePicker'].includes(selectedComponent) ? (
+                        {['RadioButton', 'TimePicker', 'DatePicker', 'DateTimePicker'].includes(selectedComponent) ? (
                           <div>
                             <Typography level="p2" fontWeight="bold">{labelText}</Typography>
                             {helperText && <Typography level="p3" mb="8px">{helperText}</Typography>}
